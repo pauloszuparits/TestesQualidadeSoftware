a5b83b916fafe3d7797921111324fbf554d8bc6e
Paulo Szuparits <pauloszuparits@hotmail.com>
2023-06-02 19:30:48 -0300

    Segundo teste feito - criar usuario

--- main.py
+++ main.py
@@ -13,6 +13,25 @@ def test_get_all_users():
     else:
         print("Teste: obter todos os usuários - FALHA")
 
+def test_create_user(user_data):
+    response = requests.post(base_url, json=user_data)
+    if response.status_code == 201:
+        print("Teste: criar usuário - SUCESSO")
+        print(response.json())
+    else:
+        print("Teste: criar usuário - FALHA")
+
 # Teste 1 - Obter todos os usuários
 print("=== Teste 1 ===")
 test_get_all_users()
+
+# Teste 2 - Criar usuário
+print("=== Teste 2 ===")
+user_data = {
+    'nome': 'João',
+    'sobrenome': 'Silva',
+    'email': 'joao@example.com',
+    'senha': 'senha123',
+    'nasc': '1990-01-01'
+}
+test_create_user(user_data)
